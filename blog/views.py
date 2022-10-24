14aa54c16066332d99594a40ea918e984e28e5ba
lbrackentx <lanasuebracken@gmail.com>
2022-10-23 20:39:07 -0500

    updated chapter 6 of blog app

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import ListView, DetailView  # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView  # new
+from django.urls import reverse_lazy  # new
+
 from .models import Post
 
 # Create your views here.
@@ -12,3 +15,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):  # new
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):  # new
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
